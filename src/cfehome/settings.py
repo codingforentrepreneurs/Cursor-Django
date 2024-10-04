abb4427c795a0c124b4ccb17d7fc406b41ea5995
justin mitchel <codingforentrepreneurs@gmail.com>
2024-10-04 21:17:46 +0000

    17 - Handling Custom Static Files

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -117,10 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "static" # /static/
 STATICFILES_DIRS = [
-    BASE_DIR.parent / "staticfiles",
+    BASE_DIR / "staticfiles",
 ]
 
 # Default primary key field type
