bf5085f56649be72b7ea500c9cefca8aad978645
justin mitchel <codingforentrepreneurs@gmail.com>
2024-10-04 21:07:30 +0000

    16 - Serving Static Files

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static" # /static/
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR.parent / "staticfiles",
 ]
 
 # Default primary key field type
