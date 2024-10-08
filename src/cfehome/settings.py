4f38366ab07c5c31644eadeb964106e4bd270b29
justin mitchel <codingforentrepreneurs@gmail.com>
2024-10-08 17:05:19 +0000

    22 - Integrate Django and Neon Postgres

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -83,6 +83,17 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = config('DATABASE_URL', default=None)
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=False,
+        )
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
